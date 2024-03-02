2c39ebd893b4aa2b198141b2ab245db41c6bc5ff
prabhattiwarii <prabhattiwari9629@gmail.com>
2024-03-02 18:42:23 +0530

    header responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import "./Header.css"
 import logo from "../../assets/logo.png"
+import menuIcon from "../../assets/menu-icon.png"
 import {Link} from 'react-scroll';
 
 const Header = () => {
     const [sticky, setSticky] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false)
     useEffect(() => {
         window.addEventListener("scroll", () => {
             window.scrollY > 50 ? setSticky(true) : setSticky(false)
         })
     }, [])
+    const toggleMenu = () => {
+        if(mobileMenu){
+            setMobileMenu(false)
+        }else{
+            setMobileMenu(true)
+        }
+    }
     return (
-        <nav className={`nav-wrap container ${sticky ? "dark" : ""}`}>
+        <nav className={`container ${sticky ? "dark" : ""}`}>
             <img src={logo} alt="Logo" className='logo' />
-            <ul className='nav-list'>
+            <ul className={`${mobileMenu ? "" : "hide-mobile-menu"}`}>
                 <li className='list'><Link to="banner" smooth={true} offset={0} duration={500}>Home</Link></li>
-                <li className='list'><Link to="programs" smooth={true} offset={0} duration={500}>Program</Link></li>
+                <li className='list'><Link to="programs" smooth={true} offset={-260} duration={500}>Program</Link></li>
                 <li className='list'><Link to="about" smooth={true} offset={0} duration={500}>About us</Link></li>
                 <li className='list'><Link to="campus" smooth={true} offset={0} duration={500}>Campus</Link></li>
                 <li className='list'><Link to="testimonial" smooth={true} offset={0} duration={500}>Testimonail</Link></li>
                 <li className='list'><Link to="contact" smooth={true} offset={0} duration={500}><button className='btn'>Contact us</button></Link></li>
             </ul>
+            <img src={menuIcon} alt="menuicon" className='menu-icon' onClick={toggleMenu}/>
         </nav>
     )
 }
